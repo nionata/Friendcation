14ae7a6801bddf9924afcd1afbeedb927d1c6985
Nicholas Ionata <n.ionata129@gmail.com>
2017-09-17 00:46:18 -0400

    Added stuff and things

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,62 @@
 import React, {Component} from 'react';
 import "../assets/styling/Home.css";
+import * as firebase from 'firebase';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    //this.handleOnName = this.handleOnName.bind(this);
+    this.renderFriend = this.renderFriend.bind(this);
+
+    this.state = {
+        gId: props.match.params.id,
+        friends: [],
+    };
+  }
+
+  componentDidMount() {
+    var friends = [];
+
+    firebase.database().ref().child(this.state.gId).on('value', function(snapshot) {
+      snapshot.forEach(function(friend) {
+        friends.push({
+          fId: friend.key,
+          name: friend.val().name,
+          location: friend.val().location,
+          destination: friend.val().destination,
+        });
+      });
+
+      this.setState({friends});
+    }.bind(this));
+  }
+
+  renderFriend(friend, index) {
+    return(
+        <h4 key={index} className="Friend">{friend.name}</h4>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
         <div className="Home-header">
           <h2>Lets get this Friend-cation going!</h2>
-          <h3>Answer a few questions below and you will be all set!</h3>
+          {
+            (this.state.friends.length !== 0) ? (
+              <div className="Home-">
+                <h3>Here is who is going!</h3>
+                {this.state.friends.map(this.renderFriend)}
+                <button className="btn">Join the Friend-cation</button>
+              </div>
+            ) : (
+              <button className="btn">Get Started</button>
+            )
+          }
         </div>
         <p className="Home-intro">
-          To get started, pick your preference bellow.
+          To get started, pick your preference bellow. {this.state.gId}
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyD8AppmA_1x9EfaYCWFbgem5eZ0qMofweA",
+    authDomain: "hackmit-2017-4d102.firebaseapp.com",
+    databaseURL: "https://hackmit-2017-4d102.firebaseio.com",
+    projectId: "hackmit-2017-4d102",
+    storageBucket: "hackmit-2017-4d102.appspot.com",
+    messagingSenderId: "212707144643"
+  };
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
